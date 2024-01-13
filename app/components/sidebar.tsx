8e9959bd963937f841606b2ee19b4e636e02c8f9
xifenglai <156486579+xifenglai@users.noreply.github.com>
2024-01-13 14:21:15 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          白马AI
         </div>
         <div className={styles["白马AI"]}>
           关注公众号“珈蓝白马”了解更多
