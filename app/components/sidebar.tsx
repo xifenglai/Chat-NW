9b986662f8c0dd1eb87b2de864314347c5d871d4
xifenglai <156486579+xifenglai@users.noreply.github.com>
2024-01-13 13:19:09 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -157,8 +157,8 @@ export function SideBar(props: { className?: string }) {
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
           NextChat
         </div>
-        <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+        <div className={styles["白马AI"]}>
+          关注公众号“珈蓝白马”了解更多
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
